f030b6e5a5ad3e41f69942716f226d1916c1dc72
MrFontina <nahuelbracchitta@gmail.com>
2022-09-19 12:16:17 -0300

    problemas con el form

---
+++ registro.js
@@ -0,0 +1,35 @@
+let formRegistro = document.getElementById('formRegistro')
+
+let formMail = document.getElementById('formMail')
+let formNombre = document.getElementById('formNombre')
+let formTelefono = document.getElementById('formTel')
+
+
+let mail = document.getElementById('mail')
+let nombre = document.getElementById('nombreApellido')
+let telefono = document.getElementById('telefono')
+// let ciudad = document.getElementById('selectCiudad')
+console.log(mail.value)
+// let mail = formMail.children[1].value
+
+// let nombre = formNombre.children[1].value
+
+// let telefono = formTelefono.children[1].value
+
+// let ciudad = formRegistro.children[8].value
+
+// formRegistro.addEventListener('submit', datosForm)
+
+
+
+
+
+// function validarTelefono()
+
+
+
+// function validateEmail(mail) {
+//     let res = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//     return res.test(mail);
+//   }
+
