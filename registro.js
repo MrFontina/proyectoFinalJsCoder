bb5473a334ea48ef4a5d9dd9d90d519a0c5998f2
MrFontina <nahuelbracchitta@gmail.com>
2022-09-19 14:17:32 -0300

    terminando el registro

--- registro.js
+++ registro.js
@@ -1,35 +1,75 @@
 let formRegistro = document.getElementById('formRegistro')
-
 let formMail = document.getElementById('formMail')
 let formNombre = document.getElementById('formNombre')
 let formTelefono = document.getElementById('formTel')
 
-
 let mail = document.getElementById('mail')
 let nombre = document.getElementById('nombreApellido')
 let telefono = document.getElementById('telefono')
-// let ciudad = document.getElementById('selectCiudad')
-console.log(mail.value)
-// let mail = formMail.children[1].value
+let pais = document.getElementById('selectPais')
 
-// let nombre = formNombre.children[1].value
+const imgArg = "../img/argentina.jpg"
+const imgChile = "../img/chile.jpg"
+const imgUruguay = "../img/uruguay.jpg"
 
-// let telefono = formTelefono.children[1].value
-
-// let ciudad = formRegistro.children[8].value
-
-// formRegistro.addEventListener('submit', datosForm)
+formRegistro.addEventListener('submit', validarDatosForm)
 
 
 
 
 
-// function validarTelefono()
+function validarDatosForm(e) {
+    e.preventDefault()
+    let nombreOk = nombre.value
+    let mailOk = mail.value
+    let telefonoOk = isFinite(telefono.value) && telefono.value > 0;
+    let paisOk = (pais.value == 'Argentina' || pais.value == 'Uruguay' || pais.value == 'Chile')
+    if ((nombreOk.length == 0) || (mailOk.length == 0) || !(telefonoOk) || !(paisOk)) {
+        alert("MAL")
+
+    } else {
+        switch (pais.value) {
+            case 'Argentina':
+                swal({
+                    title: "Bienvenido:",
+                    text: nombre.value.toUpperCase(),
+                    icon: imgArg,
+                });
+                setTimeout(() => {
+                    window.location.href = "comprar.html";
+                }, 3000)
+
+                break;
+            case 'Uruguay':
+                swal({
+                    title: "Bienvenido:",
+                    text: nombre.value.toUpperCase(),
+                    icon: imgUruguay,
+                });
+                setTimeout(() => {
+                    window.location.href = "comprar.html";
+                }, 3000)
+                break;
+            case 'Chile':
+                swal({
+                    title: "Bienvenido:",
+                    text: nombre.value.toUpperCase(),
+                    icon: imgChile,
+                });
+                setTimeout(() => {
+                    window.location.href = "comprar.html";
+                }, 3000)
+                break;
+
+            default:
+                break;
+        }
+    }
+
+}
 
 
 
-// function validateEmail(mail) {
-//     let res = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-//     return res.test(mail);
-//   }
-
+// function redirigir () = {
+//     window.location.href = "https://www.google.com" 
+// }
