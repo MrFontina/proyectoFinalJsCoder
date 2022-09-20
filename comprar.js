60db56cb981851d7ce5725d5a7c79a4616c2265e
MrFontina <nahuelbracchitta@gmail.com>
2022-09-20 16:17:01 -0300

    ya funciona la operacion, pero me falta asincronarla

--- comprar.js
+++ comprar.js
@@ -17,13 +17,51 @@ obtenerDatosDelLocalStorage.forEach((prod) => {
 })
 
 
+let precio = 2
 
+let dolarblue;
+
+let precioFinal;
 
 let apiDolarUrl = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
 
 
+let operation = (numero1, numero2, op) => {
+    return op(numero1, numero2)
+}
+
+multiplicar = (a, b) =>{
+    return(a * b)
+}
+
+
 fetch(apiDolarUrl)
     .then((response) => response.json())
-    .then((dolar) => {console.log(dolar[1].casa.venta)})
+    .then((dolar) => {dolarblue = (dolar[1].casa.venta) 
+        let dolarBlueNumber = parseFloat(dolarblue)
+        // precioFinal = operation(precio, dolarblue, (a, b) => a + b)
+        precioFinal = multiplicar(precio, dolarBlueNumber)
+        console.log(dolarblue)   
+        console.log(precioFinal)
+    })
 
 
+
+// const contenedorProductos = document.getElementById('contenedorProductos')
+
+
+
+
+
+// fetch(apiDolarUrl)
+//     .then((response) => response.json())
+//     .then((dolar) => {
+//         dolar.forEach((dolar) => {
+//             const div = document.createElement('div')
+//             div.innerHTML = `
+//             <h4>Dolar Blue </h4>
+//             <p>Venta${dolar.casa.venta}</p>            
+//             `
+//             contenedorProductos.appendChild(div)
+//         })
+//     }  ) 
