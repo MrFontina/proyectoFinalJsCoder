64dc5e02f2439067c3bff10dbc8b2a808a368c8e
MrFontina <nahuelbracchitta@gmail.com>
2022-09-22 12:25:08 -0300

    va teniendo estilo

--- comprar.js
+++ comprar.js
@@ -10,7 +10,11 @@ const btnConverter = document.getElementById('btnConverter')
 
 const contenedorComprar = document.getElementById('contenedorComprar')
 
+const contenedorIzquierdo = document.getElementById('contenedorIzquierdo')
+
 const btnFinalizar = document.getElementById('btnFinalizar')
+
+const precioEnArs = document.getElementById('precioEnArs')
 
 //imagen para la alerta
 
@@ -21,11 +25,11 @@ const imgApu = "../img/apus.jpg"
 obtenerDatosDelLocalStorage.forEach((prod) => {
     const div = document.createElement('div')
     div.classList.add('productoFinal')
-    div.innerHTML = `<div><p>${prod.tipo}</p>
+    div.innerHTML = `<div><h4>${prod.tipo}</h4>
     <p>Precio en USD: ${prod.precio}</p>
     <img src=".${prod.img}">
     </div>`
-    elementosDelChango.appendChild(div)
+    contenedorIzquierdo.appendChild(div)
     precioTotal.innerText = obtenerDatosDelLocalStorage.reduce((acc, prod) => acc + prod.precio, 0)
 })
 
@@ -43,35 +47,36 @@ let precioFinal;
 let apiDolarUrl = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
 
 
-multiplicar = (a, b) =>{
-    return(a * b)
+multiplicar = (a, b) => {
+    return (a * b)
 }
 
 const getApi = async () => {
-    const response = await fetch(apiDolarUrl)
-    .then((response) => response.json())
-    .then((dolar) => {dolarblue = (dolar[1].casa.venta) 
-        let dolarBlueNumber = parseFloat(dolarblue)
-        precioFinal = multiplicar(precio, dolarBlueNumber)
-        let div = document.createElement('div')
-        div.innerHTML = `<h4>Precio final en ARS:</h4>
+    await fetch(apiDolarUrl)
+        .then((response) => response.json())
+        .then((dolar) => {
+            dolarblue = (dolar[1].casa.venta)
+            let dolarBlueNumber = parseFloat(dolarblue)
+            precioFinal = multiplicar(precio, dolarBlueNumber)
+            let div = document.createElement('div')
+            div.innerHTML = `<h6>Precio final en ARS:</h6>
         <p>${precioFinal}</p>
         `
-        contenedorComprar.appendChild(div)
+            precioEnArs.appendChild(div)
 
-        console.log(dolarblue)   
-        console.log(precioFinal)
-    })
+            console.log(dolarblue)
+            console.log(precioFinal)
+        })
 }
 
 //eventos para ejecutar la conversion a ARS y para finalizar compra
 
 btnConverter.addEventListener('click', getApi)
 
-btnFinalizar.addEventListener('click', () => {swal({
-    title: "Gracias por tu compra!!",
-    text: "Vuelvas Prontos",
-    icon: imgApu,
-}) } )
-
-
+btnFinalizar.addEventListener('click', () => {
+    swal({
+        title: "Gracias por tu compra!!",
+        text: "Vuelvas Prontos",
+        icon: imgApu,
+    })
+})
